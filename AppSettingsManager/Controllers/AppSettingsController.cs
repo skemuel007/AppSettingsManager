219c3df7f4d74f62ffa42cd9b39548e5df70d8c1
Salvation Kemuel <skemuel007@gmail.com>
2022-03-11 21:42:49 +0100

    Using GetSection.Bind class method

---
+++ AppSettingsManager/Controllers/AppSettingsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AppSettingsManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppSettingsController : ControllerBase
+    {
+        private readonly ILogger<AppSettingsController> _logger;
+        private readonly IConfiguration _config;
+        private TwilioSettings _settings;
+
+        public AppSettingsController(ILogger<AppSettingsController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+            _settings = new TwilioSettings();
+            config.GetSection("Twilio").Bind(_settings);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var sendGridKey = _config.GetValue<string>("SendGridKey");
+            var twilioAuthToken = _config.GetSection("Twilio").GetValue<string>("AuthToken");
+            //var twilioAccountSid = _config.GetValue<string>("Twilio:AccountSid");
+            var twilioAccountSid = _config.GetSection("Twilio:AccountSid").Value;
+            //var thirdLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetValue<string>("BottomLevelSetting");
+            var thirdLevelSetting = _config.GetSection("FirstLevelSetting").GetSection("SecondLevelSetting").GetSection("BottomLevelSetting").Value;
+            var twilioPhoneNumber = _settings.PhoneNumber;
+            return Ok(new
+            {
+                SendGridKey = sendGridKey,
+                TwilioAuthToken = twilioAuthToken,
+                TwilioAccountSid = twilioAccountSid,
+                ThirdLevelSetting = thirdLevelSetting,
+                TwilioPhoneNumber = twilioPhoneNumber
+            }) ;
+        }
+    }
+}
---
+++ AppSettingsManager/TwilioSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace AppSettingsManager
+{
+    public class TwilioSettings
+    {
+        public string AuthToken { get; set; }
+        public string AccountSid { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
